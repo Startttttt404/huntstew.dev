ae831d2077a8b173e2eee67d7bf408690089077a
Startttttt404 <hunterstewart505@gmail.com>
2023-05-30 12:32:22 -0500

    Feat: Mobile index page

--- index.js
+++ index.js
@@ -3,41 +3,51 @@ const fs = require('fs')
 const path = require('path')
 
 const app = express();
+const useragent = require('express-useragent');
 
 app.use(express.static(__dirname + '/public'));
+app.use(useragent.express())
 
 app.get('/', async (req, res) => {
-     fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, text) => {
-        res.statusCode = 200
-        res.send(text)
-     });
+  if(req.useragent.isMobile){
+    fs.readFile(__dirname + '/public/indexMob.html', 'utf-8', (err, text) => {
+      res.statusCode = 200
+      res.end(text)
+    });
+  }
+  else{
+    fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, text) => {
+      res.statusCode = 200
+      res.end(text)
+    });
+  }
 });
 
 app.get('/about-me', async (req, res) => {
   fs.readFile(__dirname + '/public/about-me.html', 'utf-8', (err, text) => {
      res.statusCode = 200
-     res.send(text)
+     res.end(text)
   });
 });
 
 app.get('/projects', async (req, res) => {
   fs.readFile(__dirname + '/public/projects.html', 'utf-8', (err, text) => {
      res.statusCode = 200
-     res.send(text)
+     res.end(text)
   });
 });
 
 app.get('/resume', async (req, res) => {
   fs.readFile(__dirname + '/public/resume.html', 'utf-8', (err, text) => {
      res.statusCode = 200
-     res.send(text)
+     res.end(text)
   });
 });
 
 app.get('/contact', async (req, res) => {
   fs.readFile(__dirname + '/public/contact.html', 'utf-8', (err, text) => {
      res.statusCode = 200
-     res.send(text)
+     res.end(text)
   });
 });
 
